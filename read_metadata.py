1ad3141a3040d2dfc1ba3387153da100dea442c9
mikemommsen <mikemommsen@gmail.com>
2014-03-01 10:48:07 -0600

    Update read_metadata.py

--- read_metadata.py
+++ read_metadata.py
@@ -1,6 +1,21 @@
 #mike mommsen
 import sys
+from bs4 import BeautifulSoup
+
+def readMetdata(url):
+    f = urllib2.urlopen(url)
+    content = f.read()
+    soup = BeautifulSoup(content)
+    for row in soup('table')[4].findAll('tr'):
+        tds = row('td')
+        print tds
+
 
 def main():
+    url = sys.argv[1]
+    outpath = sys.argv[2]
+    
 
 if __name__ == '__main__':
+    main()
+  
