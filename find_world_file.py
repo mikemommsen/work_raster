61083343443bd06094ce1af4d69b9ac2ff7f3e97
mikemommsen <mikemommsen@gmail.com>
2014-03-13 09:05:21 -0500

    Update find_world_file.py

--- find_world_file.py
+++ find_world_file.py
@@ -4,6 +4,7 @@ import shutil
 WORLDNAMES = ['.jgw', 'jgwx', '.tfw'] # add more later
 
 def findworldfiles(indir):
+    """"""
     mylist = []
     os.chdir(indir)
     allfiles = [os.path.splitext(x) for x in os.listdir('.')]
@@ -12,8 +13,8 @@ def findworldfiles(indir):
             mylist.append(base)
     return mylist
 
-def copygeoreferenced(indir, outdir):
-    worldfiles = findworldfiles(indir)
+def copygeoreferenced(indir, outdir, worldfiles):
+    """"""
     allfiles = os.listdir('.') # pretty sure that dir is changed for us!!
     for x in allfiles:
         base, extension = os.path.splitext(x)
@@ -22,9 +23,11 @@ def copygeoreferenced(indir, outdir):
             shutil.copyfile(x, outpath)
 
 def main():
+    """"""
     indir = r'I:\Aerials\MO\County\Pemiscot\Pemiscot59' #sys.argv[1]
     outdir = r'J:\GIS_Data\Working-MikeM\production\142358\1959' #sys.argv[2]
-    copygeoreferenced(indir, outdir)
+    worldfiles = findworldfiles(indir)
+    copygeoreferenced(indir, outdir, worldfiles)
     print True
 
 if __name__ == "__main__":
