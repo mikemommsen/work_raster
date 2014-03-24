cacb16acd21d29745433e820e91fab54b55da697
mikemommsen <mikemommsen@gmail.com>
2014-03-24 08:50:33 -0500

    Update cropping.py

--- cropping.py
+++ cropping.py
@@ -2,14 +2,29 @@ import sys
 import os
 import arcpy
 
-def getcorners(inPoint, mapwidth, mapheight, scale):
+class LinearScale:
     """"""
+    def __init__(self, domain=[0,1], outrange=[0,1], limit=False):
+        """"""
+        self.domain = domain
+        self.outrange = outrange
+        self.limitDomain = limitDomain
+        self.limitOutRange = limitOutRange
+    
+    def get(inval):
+        """"""
+
+def getcorners(inPoint, mapwidth, mapheight, scale):
+    """takes the upper left corner and returns a polygon """
+    array = arcpy.array
+    pnt = arcpy.Point()
     surfacewidth = mapwidth * scale
     surfaceheight = mapheight * scale
     nw = inPoint.X, inPoint.Y
-    ne = inPoint.X, inPoint.Y
-    se = inPoint.X, inPoint.Y
-    sw = inPoint.X, inPoint.Y
+    ne = inPoint.X - surfacewidth, inPoint.Y
+    se = inPoint.X - surfacewidth, inPoint.Y - surfaceheight
+    sw = inPoint.X, inPoint.Y - surfaceheight
+    return [nw, ne, se, sw]
 
 def main():
     arg1 = sys.argv[1]
