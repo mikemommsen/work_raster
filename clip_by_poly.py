b117ea898165b9520316be2b62bfefe35e5108a9
mikemommsen <mikemommsen@gmail.com>
2014-04-01 11:06:04 -0500

    Update clip_by_poly.py

--- clip_by_poly.py
+++ clip_by_poly.py
@@ -10,27 +10,45 @@ RASTERFORMATS = ['.tif', '.jp2', '.jpg', '.sid']#add more as they are needed - w
 def clip_by_polyLayer(clipper, inraster, outdir):
     """takes a poly layer, loops through it and creates a clip of
     the image for each polygon in the clip layer named by the name field"""
+    # start up a cursor to loop through the clipper feature
     cur = arcpy.SearchCursor(clipper)
-    xlist, ylist = [], []
+    # grab the raster extent to compare with the extent of the clipping polygons
     desc = arcpy.Describe(inraster)
     rasterExtent = desc.extent
+    # we do this inside a try clause in case there is a problem so we can still delete the cursor in a finally loop
     try:
+        # loop through each row of the feature class
         for r in cur:
-            order = r.name #os.path.split(inraster)[1]
+            # grab the name - this should maybe be paramaterized
+            order = r.name
+            # the geog column is usually SHAPE so grab the geog
             geog = r.getValue('SHAPE')
+            # grab the extent of the geography
             rawextent = geog.extent
+            # this line below seems to cause some problems with .sid files
+            # it asks if the raster contains the clipping box
             if rasterExtent.contains(rawextent):
+                # join together the extent to throw into the clipper
                 extent = ' '.join(map(str,(rawextent.XMin, rawextent.YMin,rawextent.XMax, rawextent.YMax)))
+                # this line below makes a different folder for each order or area
+                # the structure of the output has not been static, so there will be changes here in the future
                 outpath = os.path.join(outdir, order)
-                outname = os.path.splitext(os.path.split(inraster)[1])[0] + '.jp2'
+                # take the tail of the raster, remove the extension and add .jpg to make the output name
+                outname = os.path.splitext(os.path.split(inraster)[1])[0] + '.jpg')
+                # if the ouput subfolder does not exist we make it
                 if not os.path.exists(outpath):
                     os.mkdir(outpath)
+                # and now we finally do the clip
                 arcpy.Clip_management(inraster,extent,os.path.join(outpath, outname), "#", "#", "NONE")
+            # if there is no overlap print some information to the user so they can see what did not clip
+            # and try to troubleshoot on their own
             else:
-                print rasterExtent
-                print rawextent
+                print inraster, rasterExtent
+                print order, rawextent
+    # could put exception handling here
     except Exception as e:
         print e
+    # and finally always delete these to prevent any locking issues
     finally:
         del r, cur
 
@@ -38,23 +56,32 @@ def main():
     """"""
     # if there are at least 4 args (title included) then we can use them
     if len(sys.argv) >= 4:
-        outdir = sys.argv[1] #r'C:\temp\topoCrops\bulk'
-        clipper = sys.argv[2] #r'J:\GIS_Data\Working-MikeM\production\bulk_mpls\crop_boxes.shp'
-        inrasters = sys.argv[3:] #r'C:\temp\topoCrops\2003.jpg'
+        outdir = sys.argv[1]
+        clipper = sys.argv[2] 
+        inrasters = sys.argv[3:] 
+    # if there are not enough args we can use some preset ones here
     else:
         print 'not enough args, using script set presets'
-        inraster = r'C:\temp\topoCrops\2003.jpg'
+        inrasters = [r'C:\temp\topoCrops\2003.jpg']
         outdir = r'C:\temp\topoCrops\bulk'
         clipper = r'J:\GIS_Data\Working-MikeM\production\bulk_mpls\crop_boxes.shp'
+    # if the output path does not exist then we need to make it
     if not os.path.exists(outdir):
         os.mkdir(outdir)
+    # inraster will always be a list because we are taking a slice
+    # so that way we can loop through it
     for inraster in inrasters:
+        # if one of the inrasters is a dir, we loop through all of the photos in that dir
+        # i am unsure if this is the best behavior, and i have never done this up to this point
         if os.path.isdir(inraster):
             rasters = [x for x in os.listdir(inraster) if not os.path.isdir(x) and os.path.splitext(x)[1] in RASTERFORMATS]
             for raster in rasters:
                 clip_by_polyLayer(clipper, raster, indir)
+        # if it is not a dir that means we just call the clip function
         else:
             clip_by_polyLayer(clipper, inraster, indir)
+            # print out raster name so people know whats up
+            print inraster
 
 if __name__ == '__main__':
     pass
