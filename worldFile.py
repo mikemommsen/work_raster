332da9d7e9409809005ff2452d0b85a10f618da6
mikemommsen <mikemommsen@gmail.com>
2014-04-05 06:30:58 -0500

    Update worldFile.py

--- worldFile.py
+++ worldFile.py
@@ -1,6 +1,15 @@
 import sys
 import os
 
+class WorldFile(Object):
+    """"""
+    def __init__(self, raster=None):
+        """"""
+        self.raster = raster
+        self.width, self.height = read_metadata.getSize(raster)
+        self.directory, self.filename = os.path.split(raster)
+        self.basefilename, self.extension = os.path.splitext(self.filename)
+        
 
 def main():
     pass
