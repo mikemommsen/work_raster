f662b8016267a7d4b9cd49bfeb4ce67b552cfbfb
mikemommsen <mikemommsen@gmail.com>
2014-04-06 21:16:33 -0500

    Update forChris.py

--- forChris.py
+++ forChris.py
@@ -2,12 +2,16 @@ import arcpy
 import os
 import sys
 
-def process(inimage, extent, ):
+def process(toa, dem):
     #we need to
     # 1 project
     # 2 clip
     # 3 someother shit (mainly deleting some shit
-    arcpy.ProjectRaster_management("image.tif", "reproject.tif", "World_Mercator.prj",\"BILINEAR", "5", "NAD_1983_To_WGS_1984_5", "#", "#")
+    desc = arcpy.Describe(dem)
+    spref = desc.spatialReference
+    # is the extent an issue? this is only going to ive us the max mins not a real bounding box
+    extent = desc.extent
+    arcpy.ProjectRaster_management(dem, "reproject.tif", "World_Mercator.prj",\"BILINEAR", "5", "NAD_1983_To_WGS_1984_5", "#", "#")
     # make sure that we use a registration point to match the grids
     arcpy.Clip_management("image.tif",extent,"clip.gdb/clip", "#", "#", "NONE")
 
