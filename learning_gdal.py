9f0d70849409ac3d428487bc9aa1df071cd7406d
mikemommsen <mikemommsen@gmail.com>
2014-06-07 11:51:03 -0500

    Create learning_gdal.py

---
+++ learning_gdal.py
@@ -0,0 +1,16 @@
+# import statements
+import os
+import sys
+
+# these are all the imports for gdal shit
+from osgeo import gdal
+from osgeo import ogr
+from osgeo import osr
+from osgeo import gdal_array
+from osgeo import gdalconst
+
+def main():
+    inphoto = sys.argv[1]
+
+if __name__ == '__main__':
+    main()
