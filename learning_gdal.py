57bd0767ff75593e51be5055074792744b9832a9
mikemommsen <mikemommsen@gmail.com>
2014-06-07 12:27:31 -0500

    Update learning_gdal.py

--- learning_gdal.py
+++ learning_gdal.py
@@ -1,3 +1,5 @@
+# mike mommsen
+# june 2014
 # import statements
 import os
 import sys
@@ -8,6 +10,24 @@ from osgeo import ogr
 from osgeo import osr
 from osgeo import gdal_array
 from osgeo import gdalconst
+
+def openImage(inimage):
+    inDs = gdal.Open(inimage, GA_ReadOnly)
+    return inimage
+    
+# open the image
+def getSize(inimage):
+    rows = inDs.RasterYSize
+    cols = inDs.RasterXSize
+    bands = inDs.RasterCount
+    return rows, cols, bands
+
+# get the bands and block sizes
+inBand2 = inDs.GetRasterBand(2)
+inBand3 = inDs.GetRasterBand(3)
+blockSizes = inBand2.GetBlockSize()
+xBlockSize = blockSizes[0]
+yBlockSize = blockSizes[1]
 
 def main():
     inphoto = sys.argv[1]
